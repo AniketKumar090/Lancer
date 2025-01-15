435df70533d0df271cc06281f43c03d13c35c70a
AniketKumar090 <kumaraniket009@gmail.com>
2025-01-16 01:14:47 +0530

    Initial Commit

---
+++ Lancer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lancer
+//
+//  Created by Aniket Kumar on 16/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lancer/LancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LancerApp.swift
+//  Lancer
+//
+//  Created by Aniket Kumar on 16/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
